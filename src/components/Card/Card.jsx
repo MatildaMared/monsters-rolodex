d8832b2da3715e0c832abfefd8e157f0e2808f4e
Matilda Mared <matildamared@live.se>
2022-04-19 17:58:24 +0200

    add card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Card.styles.css";
+
+function Card(props) {
+    const { name, address, id } = props.kitten;
+    return (
+        <li className="card-container">
+            <img
+                alt={`kitten ${name}`}
+                src={`https://robohash.org/${id}?set=set4&size=180x180`}
+            />
+            <h2>{name}</h2>
+            <p>{address.city}</p>
+        </li>
+    );
+}
+
+export default Card;
--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,22 +1,15 @@
 import { Component } from "react";
+import Card from "../Card/Card";
+import "./CardList.styles.css";
 
 class CardList extends Component {
     render() {
         return (
-            <ul>
-                {this.props.kittens.map((kitten) => {
-                    const { id, name, city } = kitten;
-                    return (
-                        <li key={id}>
-                            <img
-                                alt={`kitten ${name}`}
-                                src={`https://robohash.org/${id}?set=set4&size=180x180`}
-                            />
-                            <h2>{name}</h2>
-                            <p>{city}</p>
-                        </li>
-                    );
-                })}
+            <ul className="card-list">
+                {this.props.kittens &&
+                    this.props.kittens.map((kitten) => {
+                        return <Card key={kitten.id} kitten={kitten} />;
+                    })}
             </ul>
         );
     }
